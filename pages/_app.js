de5d051874eccfccc363fc7a8e12df9bc9d4771c
NestorDanielGomez <nestordanielgomez@gmail.com>
2023-01-16 17:03:56 -0300

    carro terminado con localstorage

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,65 @@
-import '../styles/globals.css'
+import { useState, useEffect } from "react";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const carritoLS =
+    typeof window != "undefined"
+      ? JSON.parse(localStorage.getItem("carrito")) ?? []
+      : [];
+
+  const [carrito, setCarrito] = useState(carritoLS);
+  const [paginaLista, setPaginaLista] = useState(false);
+
+  useEffect(() => {
+    setPaginaLista(true);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+  }, [carrito]);
+
+  const agregarAlCarrito = (bebida) => {
+    if (carrito.some((bebidaState) => bebidaState.id === bebida.id)) {
+      //itero el arreglo y busco el elemento duplicado
+      const carroActualizado = carrito.map((bebidaEstado) => {
+        if (bebidaEstado.id === bebida.id) {
+          //rescribo solo la cantidad
+          bebidaEstado.cantidad = bebida.cantidad;
+          //sumo a la cantidad previa el valor que volvio a elegir
+          //bebidaEstado.cantidad += bebida.cantidad;
+        }
+        return bebidaEstado;
+      });
+      setCarrito(carroActualizado);
+    } else {
+      setCarrito([...carrito, bebida]);
+    }
+  };
+
+  const actualizarCantidad = (bebida) => {
+    const carritoActualizado = carrito.map((bebidaState) => {
+      if (bebidaState.id === bebida.id) {
+        bebidaState.cantidad = bebida.cantidad;
+      }
+      return bebidaState;
+    });
+    setCarrito(carritoActualizado);
+  };
+
+  const eliminarBebida = (id) => {
+    const carritoActualizado = carrito.filter(
+      (bebidaState) => bebidaState.id !== id
+    );
+    setCarrito(carritoActualizado);
+  };
+
+  return paginaLista ? (
+    <Component
+      {...pageProps}
+      carrito={carrito}
+      agregarAlCarrito={agregarAlCarrito}
+      actualizarCantidad={actualizarCantidad}
+      eliminarBebida={eliminarBebida}
+    />
+  ) : null;
 }
--- pages/bebidas/[url].js
+++ pages/bebidas/[url].js
@@ -1,9 +1,28 @@
+import { useState } from "react";
 import Image from "next/image";
 import Layout from "../../components/layout";
 import styles from "../../styles/bebidas.module.css";
 
-export default function Producto({ producto }) {
+export default function Producto({ producto, agregarAlCarrito }) {
+  const [cantidad, setCantidad] = useState(0);
   const { nombre, descripcion, imagen, precio } = producto[0].attributes;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (cantidad < 1) {
+      alert("cantidad no valida");
+      return;
+    }
+    const productoSeleccionado = {
+      id: producto[0].id,
+      imagen: imagen.data.attributes.url,
+      nombre,
+      precio,
+      cantidad,
+    };
+    agregarAlCarrito(productoSeleccionado);
+  };
+
   return (
     <Layout title={`Producto ${nombre}`}>
       <div className={styles.bebida}>
@@ -17,6 +36,23 @@ export default function Producto({ producto }) {
           <h3>{nombre}</h3>
           <p className={styles.descripcion}>{descripcion}</p>
           <p className={styles.precio}>${precio}</p>
+          <form action="" className={styles.formulario} onSubmit={handleSubmit}>
+            <label htmlFor="cantidad">Cantidad:</label>
+            <select
+              name="cantidad"
+              id=""
+              onChange={(e) => {
+                setCantidad(Number(e.target.value));
+              }}>
+              <option value="0">--Seleccione--</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+            <input type="submit" name="Agregar al carrito" id="" />
+          </form>
         </div>
       </div>
     </Layout>
